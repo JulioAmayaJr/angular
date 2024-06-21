1aeccea1de8f3e8e398ce0aa3b34d63b9f52d618
JulioAmayaJr <julioamaya908@gmail.com>
2024-06-21 10:26:45 -0600

    firts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+declare var $: any;
+declare function appInit([]): any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ecommerce';
+  title = 'Julio Shop';
+  constructor() {
+    setTimeout(() => {
+      appInit($);
+    }, 50); 
+  }
 }
